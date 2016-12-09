595290d2c53727f06dface96c637b8fee9b4fb29
yoobright <yxj3179@gmail.com>
2016-12-09 09:21:52 +0800

    add aux reg encode

--- encode_util.py
+++ encode_util.py
@@ -27,18 +27,20 @@ def check_mem_data_name(check_input):
     return False
 
 
-def hex2bin(input_num):
+def hex2bin(input_num, truncate=None):
     if isinstance(input_num, basestring):
         int_num = int(input_num, 16)
     else:
         int_num = int(input_num)
-    ret = int2bin(int_num)
+    ret = int2bin(int_num, truncate)
     return ret
 
 
-def int2bin(input_num):
+def int2bin(input_num, truncate=None):
     ret = '{:0>64b}'.format(
             (int(input_num) + 0x10000000000000000) & 0xffffffffffffffff)
+    if truncate:
+        ret = ret[-truncate:]
     return ret
 
 
@@ -61,3 +63,12 @@ def mem_data_addr_encode(input_str, data_dict):
     else:
         raise AsmException('memory data address encode error')
     return ret
+
+
+def aux_reg_encode(input_str, reg_dict):
+    ret = None
+    aux_reg_name = input_str[1:-1]
+    if aux_reg_name in reg_dict:
+        ret = hex2bin(reg_dict[aux_reg_name], truncate=8)
+    else:
+        raise AsmException('auxiliary register encode error')
