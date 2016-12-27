1aa7e160d41e09674a2f071d0cb3ca5ca7a61496
yoobright <yxj3179@gmail.com>
2016-12-27 10:34:37 +0800

    adjust opcode decode

---
+++ util/decode.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from decode_dict import reg_decode_dict
+from decode_dict import aux_reg_decode_dict
+from decode_dict import opcode_decode_dict
+
+
+def decode_reg(input_str):
+    ret = None
+    if input_str in reg_decode_dict:
+        ret = reg_decode_dict[input_str]
+    return ret
+
+
+def decode_aux_reg(input_str):
+    ret = None
+    if input_str in aux_reg_decode_dict:
+        ret = "[{0}]".format(aux_reg_decode_dict[input_str])
+    return ret
+
+
+def decode_opcode(input_str):
+    ret = None
+    input_str = input_str[:8] + '_' + input_str[8:9] + '_' + input_str[9:]
+    print(input_str)
+    if input_str in opcode_decode_dict:
+        ret = opcode_decode_dict[input_str]
+    return ret
+
+
+def decode_all_reg(input_str):
+    d_bits = input_str[0:6]
+    a_bits = input_str[6:12]
+    b_bits = input_str[12:]
+
+    d_decode = decode_reg(d_bits)
+    a_decode = decode_reg(a_bits)
+    b_decode = decode_reg(b_bits)
+
+    decode_list = filter(lambda x: x, [d_decode, a_decode, b_decode])
+    ret = ' '.join(decode_list).strip()
+
+    return ret
+
+
+def decode_imm(input_str):
+    ret = hex(int(input_str, 2))
+    return ret
--- util/decode_dict.py
+++ util/decode_dict.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from encode_dict import reg_encode_dict
+from encode_dict import aux_reg_encode_dict
+from encode_dict import opcode_encode_dict
 
 reg_decode_dict = {}
 for key, value in reg_encode_dict.items():
     reg_decode_dict[value] = key
 
+aux_reg_decode_dict = {}
+for key, value in aux_reg_encode_dict.items():
+    aux_reg_decode_dict[value] = key
+
+opcode_decode_dict = {}
+for key, value in opcode_encode_dict.items():
+    fun_imm_group_pair = opcode_encode_dict[key]['function'] + '_' + \
+        opcode_encode_dict[key]['imm'] + '_' + \
+        opcode_encode_dict[key]['group']
+    opcode_decode_dict[fun_imm_group_pair] = key
+
 
 if __name__ == '__main__':
     print(reg_decode_dict)
+    print(aux_reg_decode_dict)
+    print(opcode_decode_dict)
