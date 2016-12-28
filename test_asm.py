e8eedbf0b8d90ca4b6c12cf6518f16c4605442b3
yoobright <yxj3179@gmail.com>
2016-12-28 19:05:19 +0800

    add get asm file function

--- test_asm.py
+++ test_asm.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 import subprocess
+import os
+
+
+def get_asm_file(dir_name):
+    file_list = []
+    for asm_file in os.listdir(dir_name):
+        if asm_file.endswith(".asm"):
+            asm_file_name = os.path.join(dir_name, asm_file)
+            file_list.append(asm_file_name)
+    return file_list
 
 cmd_temp = "python assemble.py -i {0} -v"
-file_name = "test.txt"
+file_name = "test\\test.txt"
 cmd_str = cmd_temp.format(file_name)
 subprocess.call(cmd_str, shell=True)
+
+print(get_asm_file("test\\lenet_fixed8"))
